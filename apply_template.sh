f696d216de9ee4b075526f9c0346f1f72f58b292
Mathis Albertzard <albertzard@new-data-services.de>
2023-05-08 13:40:22 +0200

    fix: Formating

--- apply_template.sh
+++ apply_template.sh
@@ -2,13 +2,13 @@
 
 # Check that the destination directory has been provided as an argument
 if [ -z "$1" ]
- then
-     echo "Usage: $0 <destination_directory>"
-     exit 1
- fi
+then
+    echo "Usage: $0 <destination_directory>"
+    exit 1
+fi
 
- # Store the destination directory in a variable
- destination_directory=$1
+# Store the destination directory in a variable
+destination_directory=$1
 
 # Create the destination directory if it doesn't exist
 if [ ! -d "$destination_directory" ]
@@ -16,6 +16,6 @@ then
     mkdir -p "$destination_directory"
 fi
 
- 
- # Find all files in the `template` directory (and its subdirectories) that do not contain "example" in their filenames, and copy them while keeping the folder structure
- find template -type f ! -name '*example*' -exec sh -c 'mkdir -p "$0/$(dirname ${1#*/})" && cp "$1" "$0/${1#*/}"' "$destination_directory" {} \;
+
+# Find all files in the `template` directory (and its subdirectories) that do not contain "example" in their filenames, and copy them while keeping the folder structure
+find template -type f ! -name '*example*' -exec sh -c 'mkdir -p "$0/$(dirname ${1#*/})" && cp "$1" "$0/${1#*/}"' "$destination_directory" {} \;
